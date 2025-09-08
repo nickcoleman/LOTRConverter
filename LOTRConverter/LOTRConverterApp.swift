a022838ff293710d56ea23296e2a29dd221dd295
Nick Coleman <nicholasfcoleman@gmail.com>
2025-09-08 16:50:04 -0600

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Nick Coleman on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
